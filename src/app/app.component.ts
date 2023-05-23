0830c5d4b340f935e8b4643bd9af340a741f4630
zaitsev1393 <zaitsev1393@gmail.com>
2023-05-23 18:26:12 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `X`,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ssr-blog';
+  private readonly metaTagService = inject(Meta);
+  private readonly titleService = inject(Title);
+  constructor() {
+    this.titleService.setTitle('Glibrary');
+    this.metaTagService.addTags([
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Glib Zaycev' },
+      { name: 'description', content: 'Glib Zaycev | Arrested Developer' },
+    ]);
+  }
 }
