68124ae97a6ba1058fd2e15f9042998024213f2e
zaitsev1393 <zaitsev1393@gmail.com>
2023-05-23 19:08:50 +0300

    links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `xXx`,
+  imports: [CommonModule, RouterOutlet, RouterModule],
+  template: `
+    <div class="links">
+      <a [routerLink]="'blog'">Blog</a>
+      <a [routerLink]="'about'">About</a>
+    </div>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   private readonly metaTagService = inject(Meta);
   private readonly titleService = inject(Title);
-  constructor() {
+  constructor(private router: Router) {
     this.titleService.setTitle('Glibrary');
     this.metaTagService.addTags([
       { name: 'robots', content: 'index, follow' },
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { BlogListComponent } from './blog/list.component';
+import { AboutComponent } from './about/about.componen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'blog',
+        component: BlogListComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/blog',
+  },
+];
